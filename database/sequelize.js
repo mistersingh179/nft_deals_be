cbcef805b213953578ab161aa09b5a4cb95cb1b3
Mister Singh <mistersingh179@gmail.com>
2022-05-09 01:20:46 -0400

    added option to do ssl for pg on heroku

--- database/sequelize.js
+++ database/sequelize.js
@@ -1,4 +1,15 @@
-const { Sequelize } = require('sequelize');
-const sequelize = new Sequelize(process.env.DATABASE_URL);
+const { Sequelize } = require("sequelize");
+
+const dialectOptions = {};
+if (process.env.NODE_ENV == "production") {
+  dialectOptions["ssl"] = {
+    require: true,
+    rejectUnauthorized: false,
+  };
+}
+
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+  dialectOptions: dialectOptions,
+});
 
 module.exports = sequelize;
